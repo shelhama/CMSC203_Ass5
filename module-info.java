3625a8226a37502bfb671ab35079c32d746dbe20
shelhama <149015717+shelhama@users.noreply.github.com>
2024-12-09 12:28:32 -0500

    Add files via upload

---
+++ module-info.java
@@ -0,0 +1,9 @@
+module shelhama_Ass5 {
+    
+    requires javafx.controls;
+    requires javafx.fxml;
+	requires junit;
+
+    // Exporting the package so it can be used by other modules
+    exports shelhama_Ass5;
+}
